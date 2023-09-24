689f4ef92a1b12e9e871a87a59c28874f4ceac08
Chale <charlie.feijoo@gmail.com>
2023-09-23 20:56:33 -0500

    eigth commit

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 pip install venv
-python3.9 -m venv env
-env/Scripts/activate
+python3.9 -m venv venv
+venv/Scripts/activate
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
